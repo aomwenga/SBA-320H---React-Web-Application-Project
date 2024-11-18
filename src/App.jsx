cbd3c791145c3c315158197f0b87da9b8b6321b1
AOmwenga <aomwenga@gmail.com>
2024-11-17 23:09:11 -0600

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <footer>
+    <p>&copy; {new Date().getFullYear()} Kobe Bryant Shoes. All G.O.A.T'd.</p>
+  </footer>
+);
+
+export default Footer;
