c35605b5e3695c6170b4288add0ac8b67e791f20
AOmwenga <aomwenga@gmail.com>
2024-11-17 23:13:52 -0600

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
       <Footer />
     </>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h2>Welcome to the G.O.A.T Kobe Bryant's Shoes</h2>
+    <p>Explore the collection of the G.O.A.T Kobe Bryant's basketball shoes!</p>
+  </div>
+);
+
+export default Home;
